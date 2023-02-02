0925f246125ad604c2b7c0e97ee2d4a6f9247f39
iVampireSP.com <im@ivampiresp.com>
2023-02-03 01:24:23 +0800

    改进

--- routes/exports.php
+++ routes/exports.php
@@ -9,5 +9,6 @@ use Illuminate\Support\Facades\Route;
  * 例如，你可以在这里导出一个函数，让其他模块调用，从而实现模块间的数据交互。
  * 但是请注意，请求内容不能过大，必须在 5s 内完成请求，否则会导致请求失败。
  */
-// Route::apiResource('hosts', Exports\HostController::class);
-// Route::apiResource('ips', Exports\IpController::class)->only(['show', 'destroy']);
+Route::apiResource('ips', Exports\HostController::class)->only('index', 'update', 'destroy');
+Route::get('pools', Exports\PoolController::class);
+Route::get('regions', Exports\RegionController::class);
