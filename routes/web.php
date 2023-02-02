4c85bfdd6359443a97d7b913cfc0e7d19e32c7ec
iVampireSP.com <im@ivampiresp.com>
2023-02-03 01:14:43 +0800

    改进

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,13 @@ use App\Http\Controllers\PoolController;
 use App\Http\Controllers\RegionController;
 use Illuminate\Support\Facades\Route;
 
-// 登入后的路由
-Route::group(['middleware' => 'auth:admin'], function () {
-    Route::resource('regions', RegionController::class)->except(['show']);
+Route::resource('regions', RegionController::class)->except(['show']);
 
-    Route::resource('pools', PoolController::class);
-    Route::get('pools/{pool}/generate', [
-        PoolController::class,
-        'show_generate'
-    ])->name('pools.generate');
-    Route::post('pools/{pool}/generate', [PoolController::class, 'run_generate'])->name('pools.generate');
+Route::resource('pools', PoolController::class);
+Route::get('pools/{pool}/generate', [
+    PoolController::class,
+    'show_generate'
+])->name('pools.generate');
+Route::post('pools/{pool}/generate', [PoolController::class, 'run_generate'])->name('pools.generate');
 
-    Route::resource('ips', IpController::class)->except(['show']);
-});
+Route::resource('ips', IpController::class)->except(['show']);
