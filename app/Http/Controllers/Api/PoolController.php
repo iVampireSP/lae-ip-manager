50f72a47be16858268f7e7888ab145a626bfd891
iVampireSP.com <im@ivampiresp.com>
2023-02-14 19:47:06 +0800

    改进

--- app/Http/Controllers/Api/PoolController.php
+++ app/Http/Controllers/Api/PoolController.php
@@ -13,11 +13,12 @@ class PoolController extends Controller
 
     public function __invoke(Request $request): JsonResponse
     {
-
         $pools = Pool::with('region');
 
         if ($request->filled('region_id')) {
             $pools->where('region_id', $request->input('region_id'));
+        } else {
+            $pools = $pools->get();
         }
 
         return $this->success($pools);
