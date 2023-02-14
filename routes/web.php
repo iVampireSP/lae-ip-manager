3629d5da8e7bd5dda9071b0a88125b91a73a16bd
iVampireSP.com <im@ivampiresp.com>
2023-02-14 18:57:10 +0800

    改进

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,6 @@ Route::get('pools/{pool}/generate', [
     PoolController::class,
     'show_generate'
 ])->name('pools.generate');
-Route::post('pools/{pool}/generate', [PoolController::class, 'run_generate'])->name('pools.generate');
+Route::post('pools/{pool}/generate', [PoolController::class, 'run_generate']);
 
 Route::resource('ips', IpController::class)->except(['show']);
