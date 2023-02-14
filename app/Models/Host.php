c62c7f8c4720586ea1259ab1a76e5964df2de1c5
iVampireSP.com <im@ivampiresp.com>
2023-02-14 18:25:21 +0800

    改进

--- app/Models/Host.php
+++ app/Models/Host.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 class Host extends \ivampiresp\Cocoa\Models\Host
 {
 
-    public function ip() {
+    public function ip(): HasMany
+    {
         return $this->hasMany(Ip::class);
     }
 }
